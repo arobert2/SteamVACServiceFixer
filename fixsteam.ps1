071537d25934e445f2ce69ca8e89d416aada5886
Allen Roberts <allen@winboats.com>
2022-12-11 02:13:38 -0500

    init

---
+++ fixsteam.ps1
@@ -0,0 +1,18 @@
+write-host "Closing steam";
+try{
+    $steamId = (Get-Process "Steam").Id;
+    stop-process $steamId;
+    Wait-Process $steamId;
+}catch
+{
+    write-host "Steam is not open";
+}
+
+write-host "Fixing Steam vac service";
+
+start-process 'C:\Program Files (x86)\Steam\bin\steamservice.exe' {'/repair'};
+$fixId = (get-process "steamservice").Id;
+wait-process $fixId;
+
+write-host "Launching Steam";
+start-process 'C:\Program Files (x86)\Steam\steam.exe';
---
+++ runfix.bat
@@ -0,0 +1,1 @@
+Powershell.exe -ExecutionPolicy Unrestricted -command ".\fixsteam.ps1"
